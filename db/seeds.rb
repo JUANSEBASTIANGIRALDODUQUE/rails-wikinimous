9f5faa4186450e6b82626371c65a6bfc030bd887
Juan Giraldo <JUSEGIDU@HOTMAIL.COM>
2022-09-10 16:50:04 -0500

    Ejercicio 03 terminado el CRUD, falta el front

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cargando info'
+10.times do
+  Article.create(title: Faker::Name.name, content: Faker::Name.name_with_middle)
+end
+puts 'listo!'
